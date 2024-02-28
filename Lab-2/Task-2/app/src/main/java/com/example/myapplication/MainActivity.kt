f47dc507a161afa70cb0dbe3d8e1e67c7360f98b
Shazee17 <shahzaman856@gmail.com>
2024-02-28 20:59:57 +0500

    Updated Lab 2 Task 2

--- Lab-2/Task-2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Lab-2/Task-2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,24 +7,26 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import kotlin.math.round
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,16 +55,16 @@ fun LayoutPractice(){
     }
 
 
-
-
-    Row ( modifier = Modifier
-        .fillMaxSize()
+    Row (verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+        .fillMaxWidth()
         .padding(10.dp)
+
     ) {
         Image(painter = painterResource(id = R.drawable.ic_launcher_background),
             contentDescription = null,
-            modifier = Modifier.clip(RoundedCornerShape(10.dp)
-
+            modifier = Modifier
+                .clip(RoundedCornerShape(10.dp)
 
             )
         )
