5314e5ccd8954bbe9ee279c189d14d045452c2b8
Shazee17 <shahzaman856@gmail.com>
2024-02-28 19:50:53 +0500

    Updated Lab 2 Task 2

--- Lab-2/Task-2/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Lab-2/Task-2/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,14 +3,28 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import kotlin.math.round
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +36,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    LayoutPractice()
                 }
             }
         }
@@ -30,17 +44,41 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun LayoutPractice(){
+    var text by remember {
+        mutableStateOf("Name")
+    }
+    var text2 by remember {
+        mutableStateOf("Phone Number")
+    }
+
+
+
+
+    Row ( modifier = Modifier
+        .fillMaxSize()
+        .padding(10.dp)
+    ) {
+        Image(painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = null,
+            modifier = Modifier.clip(RoundedCornerShape(10.dp)
+
+
+            )
+        )
+
+        Column(modifier = Modifier.padding(10.dp)){
+            TextField(value = text, onValueChange = {text = it}, modifier = Modifier.padding(bottom = 10.dp))
+            TextField(value = text2, onValueChange = {text2 = it}, modifier = Modifier.padding(bottom = 10.dp))
+        }
+    }
 }
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Preview() {
     MyApplicationTheme {
-        Greeting("Android")
+        LayoutPractice()
     }
 }
